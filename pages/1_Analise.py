016c30f3947e7e49c9a592612db9e80af65912ee
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-08-15 21:03:57 -0300

    Update 1_Analise.py

--- pages/1_Analise.py
+++ pages/1_Analise.py
@@ -1,8 +1,6 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
