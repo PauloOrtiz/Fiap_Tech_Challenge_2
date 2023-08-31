48888464cd6ca26fcfe14e62540cbaa19b75a769
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-08-31 20:46:57 -0300

    up

--- pages/4_Prophet.py
+++ pages/4_Prophet.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from PIL import Image
 import pandas as pd
+import plotly.graph_objects as go
+import numpy as np
 
 
 st.set_page_config(page_title="Prophet", page_icon=":house:")
@@ -39,40 +41,42 @@ with tab1:
         </style>
     """, unsafe_allow_html=True)
 
-    st.title("Testando o Modelo ARIMA")
+    st.markdown(
+        """
+        # Prophet: O Oráculo Moderno
 
-    st.write("""
-    ## ARIMA: Uma Jornada no Tempo
-    ARIMA, que significa AutoRegressive Integrated Moving Average, é um modelo clássico na análise e previsão de séries temporais. Ele é como um viajante do tempo, capaz de olhar para o passado para prever o futuro.
+        Em um mundo onde os dados são abundantes e as tendências são voláteis, surge o Prophet, uma ferramenta de previsão para séries temporais desenvolvida pelo Facebook. Como um oráculo moderno, o Prophet é capaz de olhar para os padrões do passado e prever o que o futuro pode trazer.
 
-    O ARIMA usa três componentes principais para modelar uma série temporal: termos autoregressivos (AR), termos de média móvel (MA) e a ordem de diferenciação (I). Juntos, esses componentes permitem ao ARIMA capturar uma variedade de diferentes estruturas de séries temporais.
+        O Prophet é especialmente projetado para lidar com séries temporais que possuem padrões sazonais fortes e vários pontos de mudança. Se o ARIMA é um viajante do tempo, o Prophet é um vidente, capaz de entender e prever tendências, sazonalidades e feriados.
+        
+        ## Por que o Prophet é Especial?
 
-    ## Como o ARIMA funciona?
-    O ARIMA funciona encontrando padrões nos dados passados e usando esses padrões para prever o futuro. Os termos autoregressivos permitem que o modelo capture a influência dos valores passados na série temporal. Os termos de média móvel permitem que o modelo capture os erros de previsão passados. A diferenciação permite que o modelo capture tendências na série temporal.
+        O Prophet se destaca em sua capacidade de lidar com dados faltantes, tendências que mudam ao longo do tempo e até mesmo feriados! Ele foi projetado para ser flexível e intuitivo, tornando a previsão de séries temporais uma tarefa mais simples, mesmo para aqueles que não são especialistas em estatística.
 
-    ## A Fórmula do ARIMA
-    A fórmula do ARIMA é um pouco complexa, mas aqui está uma versão simplificada:
-    """)
+        ## A Magia por Trás do Prophet
 
-    with st.expander("Ver a fórmula do ARIMA"):
+        O Prophet utiliza um modelo aditivo, onde tendências não lineares são ajustadas à sazonalidade anual, semanal e aos efeitos dos feriados. Ele automaticamente detecta pontos de mudança nas tendências e permite que os usuários especifiquem suas próprias sazonalidades e feriados.
+
+        ## A Fórmula do Prophet
+
+        Embora o Prophet seja poderoso, sua fórmula é elegante em sua simplicidade. Aqui está uma visão geral:
+        """
+    )
+    with st.expander("Ver a fórmula do Prophet"):
         st.latex(r"""
-        Y_t = c + \phi_1 Y_{t-1} + \phi_2 Y_{t-2} + ... + \phi_p Y_{t-p} + \theta_1 e_{t-1} + \theta_2 e_{t-2} + ... + \theta_q e_{t-q} + e_t
+            y(t) = g(t) + s(t) + h(t) + \epsilon_t
         """)
-
-    st.write("""
-    ## Usando ARIMA em Python
-    Em Python, podemos usar a biblioteca `statsmodels` para trabalhar com o modelo ARIMA. A função `ARIMA` desta biblioteca nos permite ajustar um modelo ARIMA aos nossos dados e usar esse modelo para fazer previsões.
-
-    Agora, vamos testar o modelo ARIMA em nossos dados do Ibovespa!
-    """)
+    st.markdown("""
+        Onde:
+        - \( y(t) \) é a previsão.
+        - \( g(t) \) representa a tendência.
+        - \( s(t) \) captura a sazonalidade.
+        - \( h(t) \) representa os efeitos dos feriados.
+        - \( \epsilon_t \) é o erro.
+        """)
 with tab2:
+    pass
 
-
-    ibovespa_mensal = ibovespa['Fechamento'].resample("30D").mean()
-    ibovespa_mensal = ibovespa_mensal.to_frame(name="Fechamento")
-    model = ARIMA(ibovespa_mensal, order=(1, 2, 1))
-    res = model.fit()
-    st.write(res.summary())
 
 
 with tab3:
@@ -80,7 +84,7 @@ with tab3:
         """
         # Escolhendo Modelos
 
-        ## Modelo Arima 2 2 0
+        ## Modelo Prophet
 
         # Previsões e Erros
 
--- pages/6_Sobre.py
+++ pages/6_Sobre.py
@@ -6,3 +6,59 @@ st.set_page_config(page_title="Sobre", page_icon=":house:")
 image = Image.open("./src/img/ibovespa.jpg")
 st.image(image)
 
+
+st.markdown("""
+        <style>
+            body {
+                color: #ffffff;
+                background-color: #4B8BBE;
+            }
+            h1 {
+                color: #CD8D00;
+                text-align: center;
+            }
+            h2 {
+                color: #306998;
+            }
+            h3 {
+                color: #E3A15D;
+            }
+            p{
+                text-indent: 40px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+st.markdown("""
+    # Sobre o Projeto
+    
+    Este projeto foi desenvolvido como parte do 1º Tech Challenge da turma de Data Science da FIAP. Foi uma oportunidade incrível para aprimorar nossas habilidades em análise de dados e colaboração em equipe, e gostaríamos de expressar nossa gratidão a todos que participaram deste desafio.
+
+    ## Equipe do Projeto 
+    
+    - Amanda Bueno de Oliveira
+    - João Guilherme Simões
+    - Leonardo Fernandes de Moraes Alves
+    - Luiz Antonio Simette de Mello Campos
+    - Paulo Henrique Barbosa Ortiz de Souza
+            
+
+    ## Orientadores 
+    
+    Este projeto não teria sido possível sem a orientação de nossos professores:
+
+    - Edgard Joseph Kiriyama
+    - Matheus Pavani
+
+    Eles nos forneceram a estrutura necessária para concluir este desafio e estamos profundamente gratos pelo tempo e esforço que dedicaram à nossa aprendizagem.
+
+    ##  Agradecimentos
+
+    Também gostaríamos de agradecer aos nossos colegas de classe e a todos os envolvidos na organização deste desafio.
+        
+    ## Referências 📚
+
+    ### Em construção
+        
+    Gostaríamos de expressar nossa gratidão a todas estas fontes por disponibilizar esses dados publicamente.
+    """, unsafe_allow_html=True)
