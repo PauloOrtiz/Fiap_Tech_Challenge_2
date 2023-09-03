f4a18e160bd08ba469228b6fb93326dfae80584f
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-03 20:52:28 -0300

    Update 4_Facebook's Prophet.py

--- pages/4_Facebook's Prophet.py
+++ pages/4_Facebook's Prophet.py
@@ -83,7 +83,7 @@ with tab2:
     df = pd.DataFrame(ibovespa)
     model = Prophet()
     model.fit(df)
-    future = model.make_future_dataframe(periods=30)
+    future = model.make_future_dataframe(periods=15)
     forecast = model.predict(future)
     
    
