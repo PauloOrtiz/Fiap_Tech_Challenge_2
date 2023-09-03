9b69fba2d240ee00c0151c253f0c80883fd10399
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-03 15:50:22 -0300

    Update 1_Analise Exploratória.py

--- pages/1_Analise Exploratória.py
+++ pages/1_Analise Exploratória.py
@@ -13,14 +13,7 @@ st.set_page_config(page_title="Analise Exploratoria", page_icon=":house:")
 
 image = Image.open("./src/img/ibovespa.jpg")
 st.image(image)
-tabs_font_css = """
-<style>
-button[data-baseweb="tab"] {
-  font-size: 26px;
-  pading:0px;
-}
-</style>
-"""
+
 
 st.write(tabs_font_css, unsafe_allow_html=True)
 
