99745f4e7a25cc33d60c2137b7f8769859e6cc56
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-03 19:36:22 -0300

    Update 3_Metodologia ARIMA.py

--- pages/3_Metodologia ARIMA.py
+++ pages/3_Metodologia ARIMA.py
@@ -187,7 +187,7 @@ with tab2:
 
     A fórmula para calcular o AIC é:
 
-    [ AIC = 2k - 2$ln(L)$]
+    **[ AIC = 2k - 2$ln(L)$]**
 
     Onde:
 
@@ -205,7 +205,7 @@ with tab2:
 
     A fórmula para calcular o BIC é:
 
-    [ BIC = -2$ln(L)$ + k * $ln(n)$]
+    **[ BIC = -2$ln(L)$ + k * $ln(n)$]**
 
     Onde:
 
