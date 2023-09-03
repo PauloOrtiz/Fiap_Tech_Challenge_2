29fb62c59612eaa4829e93365eff9d3f470e579d
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-03 20:27:32 -0300

    Update 4_Facebook's Prophet.py

--- pages/4_Facebook's Prophet.py
+++ pages/4_Facebook's Prophet.py
@@ -82,7 +82,7 @@ with tab2:
     ibovespa = ibovespa.rename(columns={"Fechamento": "y"})
     ibovespa = ibovespa.rename(index={"Data": "ds"})
     st.write(ibovespa.columns)
-    st.write(ibovespa.index)
+    st.write(ibovespa.index.name)
 
     df = pd.DataFrame(ibovespa)
     
