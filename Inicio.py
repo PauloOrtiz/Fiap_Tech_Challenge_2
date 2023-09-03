6a1f251bef1fc32754a6a2eaa277696cf5fa7eb9
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-03 14:43:47 -0300

    up

--- Inicio.py
+++ Inicio.py
@@ -31,19 +31,19 @@ h2 {
 st.write("""
 # Previsão do Fechamento do Ibovespa
 ## Bem-vindo à nossa aventura no mundo financeiro!
-<p style="text-indent: 40px;"> Você já se perguntou como seria se pudéssemos prever o futuro do mercado de ações? Embora não possamos garantir previsões perfeitas, podemos usar a ciência de dados e o aprendizado de máquina para tentar prever tendências e movimentos futuros.<p> 
+Você já se perguntou como seria se pudéssemos prever o futuro do mercado de ações? Embora não possamos garantir previsões perfeitas, podemos usar a ciência de dados e o aprendizado de máquina para tentar prever tendências e movimentos futuros.
 
 ## O Ibovespa
-<p style="text-indent: 40px;">Nesta jornada, nosso protagonista é o Ibovespa, o principal indicador de desempenho das ações negociadas na B3, a bolsa de valores brasileira. Ele é como um termômetro do mercado de ações brasileiro, refletindo o desempenho médio das ações mais negociadas e mais representativas do mercado. Quando o Ibovespa sobe, isso geralmente significa que a maioria das ações está subindo. Quando cai, a maioria das ações está caindo.<p> 
+Nesta jornada, nosso protagonista é o Ibovespa, o principal indicador de desempenho das ações negociadas na B3, a bolsa de valores brasileira. Ele é como um termômetro do mercado de ações brasileiro, refletindo o desempenho médio das ações mais negociadas e mais representativas do mercado. Quando o Ibovespa sobe, isso geralmente significa que a maioria das ações está subindo. Quando cai, a maioria das ações está caindo.
 
 ## A missão
-<p style="text-indent: 40px;">Nossa missão é realizar uma análise preditiva do fechamento do Ibovespa. Vamos explorar dados históricos, mergulhar profundamente na análise desses dados e treinar modelos de aprendizado de máquina para prever o fechamento do Ibovespa. Nosso objetivo é alcançar uma precisão de 70% ou superior em nossas previsões. Esta é uma tarefa desafiadora, mas estamos prontos para enfrentá-la!<p>
+Nossa missão é realizar uma análise preditiva do fechamento do Ibovespa. Vamos explorar dados históricos, mergulhar profundamente na análise desses dados e treinar modelos de aprendizado de máquina para prever o fechamento do Ibovespa. Nosso objetivo é alcançar uma precisão de 70% ou superior em nossas previsões. Esta é uma tarefa desafiadora, mas estamos prontos para enfrentá-la!
 
 ## Os dados 
-<p style="text-indent: 40px;">Nossa aventura começa com a coleta de dados. Conseguimos obter dados históricos do Ibovespa do site Investing. Esses dados são como um diário do Ibovespa, registrando seus altos e baixos ao longo de um período significativo de 27 de dezembro de 2000 a 1º de agosto de 2023. Esses dados serão a base para nossas previsões. Imagine-os como o mapa do tesouro que nos guiará em nossa jornada.<p>
+Nossa aventura começa com a coleta de dados. Conseguimos obter dados históricos do Ibovespa atraves da bilbioteca yfinance. Esses dados são como um diário do Ibovespa, registrando seus altos e baixos ao longo de um período significativo de 02 de janeiro de 2001 a 1º de agosto de 2023. Esses dados serão a base para nossas previsões. Imagine-os como o mapa do tesouro que nos guiará em nossa jornada.
 
 ## Navegação
-<p style="text-indent: 40px;">Esta jornada é dividida em várias etapas, cada uma delas explorando um aspecto diferente do nosso estudo. Use o menu à esquerda para navegar pelas diferentes seções. Cada passo da jornada é crucial para alcançar nosso objetivo final: prever o fechamento do Ibovespa.<p>
+Esta jornada é dividida em várias etapas, cada uma delas explorando um aspecto diferente do nosso estudo. Use o menu à esquerda para navegar pelas diferentes seções. Cada passo da jornada é crucial para alcançar nosso objetivo final: prever o fechamento do Ibovespa.
 
-<p style="text-indent: 40px;">Agora, vamos embarcar nesta emocionante jornada de aprendizado de máquina juntos!<p>
+Agora, vamos embarcar nesta emocionante jornada de aprendizado de máquina juntos!
 """, unsafe_allow_html=True)
