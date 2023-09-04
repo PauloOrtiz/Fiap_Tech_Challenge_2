e42fc5abb6f6495306d0881a23ef202b251979f7
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-03 23:25:38 -0300

    Update 4_Facebook's Prophet.py

--- pages/4_Facebook's Prophet.py
+++ pages/4_Facebook's Prophet.py
@@ -310,7 +310,7 @@ with tab4:
         
         Outro fator a ser observado, é que os erros apresentam aleatoriedade, ou seja, não há como identificar padrões de sazonalidade ou tendência em sua série. 
         
-        Além disso, se a dispersão dos resíduos se ampliar ou estreitar à medida que você se move ao longo do eixo x, isso pode ser um sinal de heteroscedasticidade, o que significa que a variância dos erros não é constante. Esse comportamento também, em geral, não foi identificado na série através do gráfico
+        Além disso, se a dispersão dos resíduos se ampliar ou estreitar à medida que você se move ao longo do eixo x, isso pode ser um sinal de heteroscedasticidade, o que significa que a variância dos erros não é constante. Esse comportamento também, em geral, não foi identificado na série através do gráfico.
     """,unsafe_allow_html=True)
 
     
