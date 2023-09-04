5282ed06cc0a77265e80a74b0161dae36aa652c5
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-03 23:48:08 -0300

    up

---
+++ pages/5_Metodologia Sarimax.py
@@ -0,0 +1,32 @@
+import streamlit as st
+
+st.markdown("""
+        <style>
+            body {
+                color: #ffffff;
+                background-color: #4B8BBE;
+            }
+            h1 {
+                color: #CD8D00;
+                text-align: center;
+            }
+            h2 {
+                color: #306998;
+            }
+            h3 {
+                color: #E3A15D;
+            }
+            p{
+                text-indent: 40px;
+            }
+        </style>
+    """, unsafe_allow_html=True)
+
+st.markdown("""
+# SARIMAX 
+            
+## Essa pagina depende de como será o almoço do João amanhã para ser desenvolvida 
+            
+### Aguadando o João Almoçar.
+
+""")
