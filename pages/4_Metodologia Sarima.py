096eac45bf7816d17b4dc752022911f9ebe98c3e
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-04 21:48:19 -0300

    Update 4_Metodologia Sarima.py

--- pages/4_Metodologia Sarima.py
+++ pages/4_Metodologia Sarima.py
@@ -140,23 +140,7 @@ with tab3:
     ## Sumário Estatístico do Modelo
     Abaixo, apresentamos um resumo das estatísticas do modelo escolhido. Este sumário nos fornece informações valiosas sobre os parâmetros do modelo, sua significância estatística e outras métricas que nos ajudam a entender o desempenho do modelo.
     """)       
-    
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(1,0,1,12)).fit(ds=-1)
-    st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(1, 0, 0, 12)).fit(ds=-1)
-    st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(0, 0, 1, 12)).fit(ds=-1)
-    st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(0, 0, 0, 12)).fit(ds=-1)
-    st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(1, 0, 1, 12)).fit(ds=-1)
-    st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(1, 1, 1, 12)).fit(ds=-1)
-    st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(0, 1, 1, 12)).fit(ds=-1)
-    st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(1, 1, 0, 12)).fit(ds=-1)
-    st.write(model1.summary())
+        
     model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(0, 1, 0, 12)).fit(ds=-1)
     st.write(model1.summary())
 
