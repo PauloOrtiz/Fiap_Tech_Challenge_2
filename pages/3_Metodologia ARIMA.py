b7cd0a4e0afc08a8ed02a9456c07f8a9820b9d51
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-04 22:24:18 -0300

    Update 3_Metodologia ARIMA.py

--- pages/3_Metodologia ARIMA.py
+++ pages/3_Metodologia ARIMA.py
@@ -373,9 +373,9 @@ with tab5:
 
     st.write("""
     
-    - **gráfico 1** -> Resíduos padronizados 1 passo a frente, não apresentam estrutura obvia.
-    - **gráfico 2** ->  Distribuição normal de resíduos - A distribuição de resíduos esta bem ajustada a distribuição normal dos dados.
-    - **gráfico 3** ->  Normal Q-Q - Os pontos estão sobrepostos a linha vermelha, o que significa que o resíduos seguem um distribuição normal.
+    - **gráfico 1** -> Residuos padronizados 1 passo a frente, não apresentam estrutura óbvia.
+    - **gráfico 2** -> Distribuição normal de resíduos - A distribuição de resíduos está bem ajustada a distribuição normal dos dados.
+    - **gráfico 3** -> Normal Q-Q - Os pontos estão sobrepostos a linha vermelha, o que significa que o resíduos seguem uma distribuição normal.
     - **gráfico 4** -> Mede o  ACF dos resíduos onde pelo menos 95% dos lags não podem ser significativos e estão dentro da faixa significância
 
     """,unsafe_allow_html=True)
