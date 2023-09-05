b4bc0ef902dd0e1fe57a4e9c0bb2b7ce4f94961c
Paulo Ortiz <48257470+PauloOrtiz@users.noreply.github.com>
2023-09-04 21:43:12 -0300

    Update 4_Metodologia Sarima.py

--- pages/4_Metodologia Sarima.py
+++ pages/4_Metodologia Sarima.py
@@ -145,7 +145,7 @@ with tab3:
     st.write(model1.summary())
     model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(1, 0, 0, 12)).fit(ds=-1)
     st.write(model1.summary())
-    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=((0, 0, 1, 12)).fit(ds=-1))
+    model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(0, 0, 1, 12)).fit(ds=-1)
     st.write(model1.summary())
     model1 = SARIMAX(ibovespa['Fechamento'],order=(0,1,0), seasonal_order=(0, 0, 0, 12)).fit(ds=-1)
     st.write(model1.summary())
